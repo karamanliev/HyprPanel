29af4bf756a2a76d512e1518dfa9f79a2322af77
Hristo Karamanliev <karamanliev@gmail.com>
2024-08-28 20:42:55 +0300

    add cpu temp to sensors on the bar

--- modules/bar/stats/index.ts
+++ modules/bar/stats/index.ts
@@ -2,12 +2,38 @@ import { Stats as StatsMenu } from "../../menus/dashboard/stats/index";
 const Stats = () => {
   const { cpu, ram } = StatsMenu(true);
 
+  const temp = Variable(0, {
+    poll: [
+      2000,
+      "sensors -j",
+      (out) => {
+        if (typeof out !== "string") {
+          return 0;
+        }
+
+        const sensors = JSON.parse(out);
+
+        if (sensors === undefined) {
+          return 0;
+        }
+
+        const coreTemp = sensors["k10temp-pci-00c3"];
+
+        if (coreTemp === undefined) {
+          return 0;
+        }
+
+        return coreTemp["Tctl"]["temp1_input"];
+      },
+    ],
+  });
+
   return {
     component: Widget.Box({
       class_name: "language",
       children: [
         Widget.Icon({
-          class_name: "bar-button-icon language txt-icon bar",
+          class_name: "bar-button-icon network",
           icon: "/usr/share/icons/Papirus/16x16/panel/indicator-sensors-cpu.svg",
         }),
         Widget.Label({
@@ -16,13 +42,24 @@ const Stats = () => {
           label: cpu.bind("value").as((v) => `${Math.floor(v * 100)}%`),
         }),
 
+        Widget.Icon({
+          css: "margin-right: -3px; margin-top: 2px;",
+          class_name: "bar-button-icon network",
+          icon: "temp-symbolic",
+        }),
+        Widget.Label({
+          class_name: "bar-button-label language",
+          css: "margin-right: 16px;",
+          label: temp.bind("value").as((v) => `${Math.floor(v)}°C`),
+        }),
+
         // Widget.Label({
         //   class_name: "bar-button-label language",
         //   label: "    ",
         // }),
 
         Widget.Icon({
-          class_name: "bar-button-icon language txt-icon bar",
+          class_name: "bar-button-icon network",
           icon: "/usr/share/icons/Papirus/16x16/panel/indicator-sensors-memory.svg",
         }),
         Widget.Label({
