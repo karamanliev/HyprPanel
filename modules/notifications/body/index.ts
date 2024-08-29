f8b0a385665563c4061e35e092623de0a35be9f7
Hristo Karamanliev <karamanliev@gmail.com>
2024-08-29 15:03:45 +0300

    fix notification if including markup

--- modules/notifications/body/index.ts
+++ modules/notifications/body/index.ts
@@ -18,6 +18,8 @@ export const Body = (notif: Notification) => {
                 wrap: true,
                 class_name: "notification-card-body-label",
                 label: notif["body"],
+            }).on("realize", self => {
+                self.set_markup(notif["body"])
             }),
         ],
     });
